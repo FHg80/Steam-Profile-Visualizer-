b4fc57c4490103e916964a838e35ab3ddfac24e9
Pedro Barros <pedro.de.barros.alves@gmail.com>
2026-04-26 14:14:28 -0300

    add last login vis

--- profile_visualizer.c
+++ profile_visualizer.c
@@ -75,20 +75,32 @@ int main() {
                 const cJSON *personaname = cJSON_GetObjectItemCaseSensitive(player, "personaname");
                 const cJSON *realname = cJSON_GetObjectItemCaseSensitive(player, "realname");
                 const cJSON *loccountrycode = cJSON_GetObjectItemCaseSensitive(player, "loccountrycode");
+                const cJSON *profileurl = cJSON_GetObjectItemCaseSensitive(player, "profileurl");
                 const cJSON *timecreated = cJSON_GetObjectItemCaseSensitive(player, "timecreated");
+                const cJSON *lastlogoff = cJSON_GetObjectItemCaseSensitive(player, "lastlogoff");
                 const cJSON *personastate = cJSON_GetObjectItemCaseSensitive(player, "personastate");
 
-                char tempo[80];
-                time_t t = (time_t)timecreated->valuedouble;
-                struct tm *tm_data = localtime(&t);
+                char tempo_criacao[80];
+                char tempo_logoff[80];
 
-                strftime(tempo, sizeof(tempo), "%d/%m/%Y às %H:%M:%S", tm_data);
+                time_t t_creation = (time_t)timecreated->valuedouble;
+                struct tm *tm_data_creation = localtime(&t_creation);
+                strftime(tempo_criacao, sizeof(tempo_criacao), "%d/%m/%Y às %H:%M:%S", tm_data_creation);
+
+                time_t t_logoff = (time_t)lastlogoff->valuedouble;
+                struct tm *tm_data_logoff = localtime(&t_logoff);
+                strftime(tempo_logoff, sizeof(tempo_logoff), "%d/%m/%Y às %H:%M:%S", tm_data_logoff);
+
+                
+                
                 
                 if(cJSON_IsString(personaname) && (personaname->valuestring != NULL)) {
                     printf("Nome do perfil: %s\n", personaname->valuestring);
                     printf("Nome real: %s\n", realname->valuestring);
                     printf("País: %s\n", loccountrycode->valuestring);
-                    printf("Conta criada em: %s\n", tempo);
+                    printf("URL do perfil: %s\n", profileurl->valuestring);
+                    printf("Último login em: %s\n", tempo_logoff);
+                    printf("Conta criada em: %s\n", tempo_criacao);
 
                     if(personastate->valueint == OFFLINE) {
                         printf("Status: Offline\n");
