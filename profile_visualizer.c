cebdee5695963ff432902fa17bdba6e27edecba7
Pedro Barros <pedro.de.barros.alves@gmail.com>
2026-04-27 10:54:57 -0300

    add profile level vis

--- profile_visualizer.c
+++ profile_visualizer.c
@@ -62,6 +62,7 @@ int main() {
     char basic_profile_url[256];
     char recent_games_url[256];
     char games_owned_url[256];
+    char steam_level_url[256];
 
     snprintf(basic_profile_url, sizeof(basic_profile_url), "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=%s&steamids=%s",
     api_key, steamid);
@@ -72,16 +73,21 @@ int main() {
     snprintf(games_owned_url, sizeof(games_owned_url), "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=%s&steamid=%s&include_appinfo=true",
     api_key, steamid);
 
+    snprintf(steam_level_url, sizeof(steam_level_url), "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=%s&steamid=%s",
+    api_key, steamid);
+
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *handle =  curl_easy_init();
 
     struct memory profileChunk = fetch_url(handle, basic_profile_url);
     struct memory recentGamesChunk = fetch_url(handle, recent_games_url);
     struct memory ownedGamesChunk = fetch_url(handle, games_owned_url);
+    struct memory levelChunk = fetch_url(handle, steam_level_url);
 
     cJSON *basic_profile_json = cJSON_Parse(profileChunk.response);
     cJSON *recent_games_json = cJSON_Parse(recentGamesChunk.response);
     cJSON *owned_games_json = cJSON_Parse(ownedGamesChunk.response);
+    cJSON *profile_level_json = cJSON_Parse(levelChunk.response);
 
     if(basic_profile_json) {
         const cJSON *response = cJSON_GetObjectItemCaseSensitive(basic_profile_json, "response");
@@ -178,9 +184,18 @@ int main() {
         }
     }
 
+    if(profile_level_json) {
+        const cJSON *response = cJSON_GetObjectItemCaseSensitive(profile_level_json, "response");
+        const cJSON *player_level = cJSON_GetObjectItemCaseSensitive(response, "player_level");
+
+        printf("------------------------------\n");
+        printf("Nível do perfil: %i\n", player_level->valueint);
+    }
+
     free(recentGamesChunk.response);
     free(profileChunk.response);
     free(ownedGamesChunk.response);
+    free(levelChunk.response);
 
     curl_easy_cleanup(handle);
     
